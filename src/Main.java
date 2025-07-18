17dc96a169ee12a7fc67cbd0d2cc1085acd6d6d9
Vologdin Roman <vologdinroman@yandex.ru>
2025-07-18 22:30:47 +0800

    Add new comment

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,15 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.util.Scanner; // Вызываем утилиту для считывания ввода данных
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+class scanner { //Обозначаем класс, присваиваем имя
+
+    public static void main(String[] args) {    //Обозначаем метод или точка входа для JVM
+
+        try(Scanner scanner = new Scanner( System.in )) //Чтение из системного ввода с автозакрытием (try)
+        {
+            String usersName; //переменная, возвращает имя пользователя из "userName"
+            System.out.print("Пожалуйста введите Ваше имя: ");  //Выводим строку
+            usersName = scanner.next(); //Ждём ввода имени
+            System.out.println("Привееет, " + usersName+"! ");  //Выводим новую строку с приветствием
+               }
     }
 }
